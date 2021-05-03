915b305cbeac50b57eb605a183bb28e32bf03e00
Beibarys <beibarys1927@gmail.com>
2021-05-03 09:26:13 +0600

    Initial commit

---
+++ Interface_builder_basics_task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Interface_builder_basics_task
+//
+//  Created by Бейбарыс Шагай on 3/8/21.
+//  Copyright © 2021 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
